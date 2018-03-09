d25c2e0a8467f4c61b93c67b50c4dc7e995161e2
bilalAchahbar <36499520+bilalAchahbar@users.noreply.github.com>
2018-03-09 14:00:35 +0100

    Changehostnaam.sh

---
+++ bash scripts/hostname.sh
@@ -0,0 +1,6 @@
+
+# Change the hostname
+sudo hostnamectl --transient set-hostname $hostname
+sudo hostnamectl --static set-hostname $hostname
+sudo hostnamectl --pretty set-hostname $hostname
+sudo sed -i s/raspberrypi/$hostname/g /etc/hosts
