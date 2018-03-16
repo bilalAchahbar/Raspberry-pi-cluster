35eabb6178cd1b3eccd81feada43d9df9000114f
bilalAchahbar <36499520+bilalAchahbar@users.noreply.github.com>
2018-03-16 15:06:15 +0100

    Delete hostname.sh

--- bash scripts/hostname.sh
+++
@@ -1,6 +0,0 @@
-
-# Change the hostname
-sudo hostnamectl --transient set-hostname $hostname
-sudo hostnamectl --static set-hostname $hostname
-sudo hostnamectl --pretty set-hostname $hostname
-sudo sed -i s/raspberrypi/$hostname/g /etc/hosts
